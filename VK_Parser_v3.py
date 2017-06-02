5ff178ae7bf1f88e03292c3927a84bf3ebb81830
toxismart <leninanton@gmail.com>
2017-06-02 18:41:11 +0300

    v3

---
+++ VK_Parser_v3.py
@@ -0,0 +1,35 @@
+import vk
+import json
+import time
+import sys
+session = session = vk.AuthSession('App', 'Login', 'Pass')
+api = vk.API(session, v='3.0', lang='ru', timeout=10)
+id1 = input("ID to parse: ")
+values = int(id1)
+profile = api.users.get(user_ids=values, fields=('uid','deactivated'))
+friends = api.friends.get(user_id=values, fields=('user_id', 'city','domain'))
+lvl = input('Depth: ')
+max_depth = int(lvl)
+
+def profile_json(data, id_name):
+    with open('%d.json' % id_name, 'w', encoding="utf-8") as file:
+        json.dump(data, file, indent=2, ensure_ascii=False)
+
+temp = []
+
+def parse(level, values, friends):
+    if level<max_depth:
+        friends = api.friends.get(user_id=values, fields=('user_id', 'city','domain'))
+        time.sleep(5)
+        for value in friends:
+            values = value['uid']
+            temp.append(values)
+            profile_json(value, value['uid'])
+            temp.pop()
+            parse(level+1, values, friends)
+
+def main():
+    parse(0, values, friends)
+
+if __name__ == '__main__':
+    main()
